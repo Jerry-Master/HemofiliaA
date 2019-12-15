58738fc943d611a07ffdaa6a7d826e9accf5f3be
Jose Pérez Cano <jose.perez.cano@est.fib.upc.edu>
2019-12-15 10:58:42 +0100

    changes

--- kmeans.R
+++ kmeans.R
@@ -14,29 +14,25 @@ for (i in 2:11){
   withins <- c(withins, sum(clusters$withinss)/39)
 }
 plot(c(2:11), withins, type="l")
+# Choose a k
+cluster3 <- kmeans(prob.km, 3)
 
 # Generate a "confidence" interval
-desv <- apply(clusters5$centers, 2, sd)
-med <- apply(clusters5$centers, 2, mean)
-conf.down <- med - 1.75*desv
-conf.up <- med + 1.75*desv
+desv <- apply(clusters3$centers, 2, sd)
+med <- apply(clusters3$centers, 2, mean)
+conf.down <- med - 1.1547*desv
+conf.up <- med + 1.1547*desv
 
 # Get gens out of this interval
-gen1 <- clusters5$centers[1,clusters5$centers[1,] < conf.down | clusters5$centers[1,] > conf.up] %>% names()
-gen2 <- clusters5$centers[2,clusters5$centers[2,] < conf.down | clusters5$centers[2,] > conf.up] %>% names()
-gen3 <- clusters5$centers[3,clusters5$centers[3,] < conf.down | clusters5$centers[3,] > conf.up] %>% names()
-gen4 <- clusters5$centers[4,clusters5$centers[4,] < conf.down | clusters5$centers[4,] > conf.up] %>% names()
-gen5 <- clusters5$centers[5,clusters5$centers[5,] < conf.down | clusters5$centers[5,] > conf.up] %>% names()
+gen1 <- clusters3$centers[1,clusters3$centers[1,] < conf.down | clusters3$centers[1,] > conf.up] %>% names()
+gen2 <- clusters3$centers[2,clusters3$centers[2,] < conf.down | clusters3$centers[2,] > conf.up] %>% names()
+gen3 <- clusters3$centers[3,clusters3$centers[3,] < conf.down | clusters3$centers[3,] > conf.up] %>% names()
 
 # Check that they are disjoint sets
-gen1[!(gen1 %in% gen2) && !(gen1 %in% gen3) && !(gen1 %in% gen4) && !(gen1 %in% gen5)] %>% length() == length(gen1)
-gen2[!(gen2 %in% gen1) && !(gen2 %in% gen3) && !(gen2 %in% gen4) && !(gen2 %in% gen5)] %>% length() == length(gen2)
-gen3[!(gen3 %in% gen1) && !(gen3 %in% gen2) && !(gen3 %in% gen4) && !(gen3 %in% gen5)] %>% length() == length(gen3)
-gen4[!(gen4 %in% gen1) && !(gen4 %in% gen2) && !(gen4 %in% gen3) && !(gen4 %in% gen5)] %>% length() == length(gen4)
-gen5[!(gen5 %in% gen1) && !(gen5 %in% gen2) && !(gen5 %in% gen3) && !(gen5 %in% gen4)] %>% length() == length(gen5)
+gen1[!(gen1 %in% gen2) && !(gen1 %in% gen3)] %>% length() == length(gen1)
+gen2[!(gen2 %in% gen1) && !(gen2 %in% gen3)] %>% length() == length(gen2)
+gen3[!(gen3 %in% gen1) && !(gen3 %in% gen2)] %>% length() == length(gen3)
 
-write.csv(gen1, "gen1.csv")
-write.csv(gen2, "gen2.csv")
-write.csv(gen3, "gen3.csv")
-write.csv(gen4, "gen4.csv")
-write.csv(gen5, "gen5.csv")
+write.csv(gen1, "gen1.csv", quote=F)
+write.csv(gen2, "gen2.csv", quote=F)
+write.csv(gen3, "gen3.csv", quote=F)
